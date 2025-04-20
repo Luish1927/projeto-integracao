f972df554a1cdd14f7776dae7191093b083790ac
LuisH <mr.luishenriquegd@icloud.com>
2025-04-20 00:48:10 -0300

    logging changes

--- main.py
+++ main.py
@@ -9,16 +9,9 @@ from dotenv import load_dotenv
 import os
 
 
-# Configuração do log
-logging.basicConfig(filename='processamento.log', level=logging.DEBUG,
+logging.basicConfig(filename='processing.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-# Função para limpar valores de float inválidos
-# def limpar_json(dado):
-#     if isinstance(dado, float) and (dado == float('inf') or dado == float('-inf') or np.isnan(dado)):
-#         return None
-#     return dado
 
 
 def float_to_string(barcode):
@@ -104,7 +97,8 @@ def stock_formater(price):
     """
     if pd.notnull(price):
         return float(price)
-    else: return None
+    else: 
+        return None
 
 
 def measure_parser(name):
@@ -145,7 +139,7 @@ def measure_parser(name):
 
 def infer_unit_type(name):
     """
-    This function is responsible for parsing the product's name to infer the unit type. It uses regex to identify patterns such as 'uni, unidade' or numbers followed by 'ml, l, kg, g' to return the unit_type as 'UNI'. For times when the product's name contains 'kg, g' but not in the context of a size (like 'tam G') or it doesn't have a number before, it returns 'KG'.
+    This function is responsible for parsing the product's name to infer the unit type. It uses regex to identify patterns such as 'uni, unidade' or numbers followed by 'ml, l, kg, g' to return the unit_type as 'UNI'. For times when the product's name contains 'kg, g' it returns 'KG'.
     The last case considers the product's name with more than 4 words as 'UNI' to avoid false positives.
 
     :param name: str - The name of the product to be parsed.
@@ -163,7 +157,7 @@ def infer_unit_type(name):
     if re.search(r'\d+(kg|g)\b', clean_name):
         return "UNI"
 
-    if re.search(r'\b(kg|g)\b', clean_name): # test: this regex ends up capturing cases like "fralda tam G"
+    if re.search(r'\b(kg|g)\b', clean_name):
         return "KG"
 
     if not (re.search(r'\b(kg|g)\b', clean_name)) or len(words) >= 4:
@@ -180,7 +174,7 @@ def process_csv_to_batches(input_path):
     :param input_path: str - The file path to the input CSV file.
     :return: list - A list of dictionaries, where each dictionary represents a batch of products.
     """
-
+    logging.info("Starting process_csv_to_batches function.")
     df = pd.read_csv(input_path, sep=';')
 
     unit_types = []
@@ -189,12 +183,10 @@ def process_csv_to_batches(input_path):
     widths = []
     heights = []
 
-    # Iterate over the 'Nome' column to extract unit types and measurements
     for name in df['Nome']:
         unit_type = infer_unit_type(name)
         measures = measure_parser(name)
 
-        # Append the extracted data to the respective lists
         unit_types.append(unit_type)
         weights.append(measures['weight'])
         lengths.append(measures['length'])
@@ -209,43 +201,39 @@ def process_csv_to_batches(input_path):
     df['height'] = heights
 
     # Format and clean other columns in the DataFrame
-    df['name'] = df['Nome'].apply(lambda x: str(x))  # Ensure product names are strings
-    df['barcodes'] = df['Código de barras'].apply(float_to_string)  # Convert barcodes to strings
-    df['barcodes'] = df['barcodes'].apply(lambda x: format_to_ean(x[0]) if x else [])  # Format barcodes to EAN standards
-    df['promo_price'] = df['Promocao'].apply(promo_price_formater)  # Format promotional prices
-    df['price'] = df['Preço regular'].apply(price_formater)  # Format regular prices
-    df['visible'] = df['ativo']  # Map visibility status directly
-    df['internal_code'] = df['Código interno'].apply(internal_code_formater)  # Format internal codes
-    df['stock'] = df['estoque'].apply(stock_formater)  # Format stock values
-    # Set fixed promotional start and end dates (hardcoded for now)
+    df['name'] = df['Nome'].apply(lambda x: str(x))  
+    df['barcodes'] = df['Código de barras'].apply(float_to_string)  
+    df['barcodes'] = df['barcodes'].apply(lambda x: format_to_ean(x[0]) if x else [])  
+    df['promo_price'] = df['Promocao'].apply(promo_price_formater) 
+    df['price'] = df['Preço regular'].apply(price_formater) 
+    df['visible'] = df['ativo']  
+    df['internal_code'] = df['Código interno'].apply(internal_code_formater)  
+    df['stock'] = df['estoque'].apply(stock_formater) 
+    
     df['promo_end_at'] = df["Data termino promocao"].apply(lambda x: "2020-03-31T23:59:59.672000-03:00")
     df['promo_start_at'] = df["Data termino promocao"].apply(lambda x: "2020-03-31T23:59:59.672000-03:00")
 
-    # Drop source columns that are no longer needed after processing
+    
     source_columns = ['Nome', 'Código de barras', 'Promocao', 'Preço regular', 'ativo', 'Código interno', 'estoque', 'Data termino promocao']
     df.drop(columns=source_columns, inplace=True)
-
-    # Reorder the columns in the desired output format
     new_colums = ['internal_code', 'name', 'unit_type', 'price', 'visible', 'stock', 'barcodes', 'promo_price', 'weight', 'length', 'width', 'height', 'promo_end_at', 'promo_start_at']
     df = df[new_colums]
 
-    # Replace NaN values with None for compatibility with JSON serialization
+    
     df.replace({np.nan: None}, inplace=True)
 
-    # Create batches of 1000 products for further processing
+    
     batches = []
     output_dir = 'batches'
-    os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)  
     
     for i in range(0, len(df), 1000):
-        # Convert each batch of rows into a dictionary and append to the list of batches
         batch = {"products": df[i:i+1000].apply(lambda row: row.to_dict(), axis=1).tolist()}
         batches.append(batch)
-        
-        # Save each batch as a JSON file
         with open(f'{output_dir}/batch_{i//1000 + 1}.json', 'w', encoding='utf-8') as json_file:
             json.dump(batch, json_file, ensure_ascii=False, indent=4)
 
+    logging.info("Finished process_csv_to_batches function.")
     return batches
 
 
@@ -255,37 +243,32 @@ def send_to_api(batches):
 
     :param batches: list - A list of dictionaries, where each dictionary represents a batch of products to be sent.
     """
+    logging.info("Starting send_to_api function.")
+    load_dotenv() 
 
-    load_dotenv() # Load environment variables from a .env file
-
-    # Define the API endpoint URL
+   
     url = 'https://api.instabuy.com.br/store/products'
-    api_key = os.getenv('API_KEY')  # Load the API key from environment variables
+    api_key = os.getenv('API_KEY')  
 
-    # Define the headers for the API request, including the API key for authentication
+    
     headers = {
         'Content-Type': 'application/json',
         'api-key': api_key
     }
 
-    # Iterate over the batches, sending each one to the API
     for i, batch in enumerate(batches, start=1):
         try:
-            # Send a PUT request to the API with the batch data serialized as JSON
             response = requests.put(url, headers=headers, data=json.dumps(batch))
 
-            # Check if the request was successful (HTTP status code 200)
             if response.status_code == 200:
-                print(response.json())  # Print the API's response for debugging purposes
-                logging.info(f'Batch {i} sent succesfully.')  # Log a success message
+                print(response.json())  
+                logging.info(f'Batch {i} sent succesfully.')  
             else:
-                # Log an error message if the API returns a non-200 status code
                 logging.error(f'Batch {i}: Error while sending - {response.text}')
         except Exception as e:
-            # Log any exceptions that occur during the request
             logging.error(f'Batch {i}: Exception while sending - {str(e)}')
 
-
+    logging.info("Finished send_to_api function.")
 
 batches = process_csv_to_batches("raw-bronze/items.csv")
 send_to_api(batches)
